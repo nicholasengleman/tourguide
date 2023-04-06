9cf335681e91615132778f0487de90b34c562a14
Nicholas Engleman <nicholas.engleman@ajc.com>
2023-04-05 22:01:05 -0400

    Added follow up questions

--- src/screens/AttractionScreen.tsx
+++ src/screens/AttractionScreen.tsx
@@ -1,7 +1,14 @@
 <script src='http://192.168.1.16:8097'></script>;
 
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, SafeAreaView, ScrollView, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  Pressable,
+} from 'react-native';
 import { Spinner } from '@ui-kitten/components';
 import OpenAIService from '../services/OpenAIService';
 
@@ -12,13 +19,40 @@ const AttractionScreen: React.FC<AttractionScreenProps> = ({ route }) => {
 
   useEffect(() => {
     const fetchAttraction = async () => {
-      const attraction = await OpenAIService.getDescription(name);
-      console.log(attraction.content);
-      setDescription(attraction?.content?.description);
-      setQuestions(attraction?.content?.questions);
+      try {
+        const attraction = await OpenAIService.getDescription(name);
+        const data = JSON.parse(attraction?.content);
+        if (data?.description) {
+          setDescription(data?.description);
+        }
+
+        if (data?.questions) {
+          setQuestions(data?.questions);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     };
     fetchAttraction();
   }, [name]);
+
+  const getNewDescription = async (question: string) => {
+    setDescription('');
+
+    try {
+      const attraction = await OpenAIService.askNewQuestions(question);
+      const data = JSON.parse(attraction?.content);
+      if (data?.description) {
+        setDescription(data?.description);
+      }
+
+      if (data?.questions) {
+        setQuestions(data?.questions);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.main}>
@@ -35,7 +69,13 @@ const AttractionScreen: React.FC<AttractionScreenProps> = ({ route }) => {
             </ScrollView>
             <View style={styles.questionContainer}>
               {questions?.map((question) => (
-                <Text style={styles.description}>{question}</Text>
+                <Pressable
+                  style={styles.questionBtn}
+                  onPress={() => getNewDescription(question)}
+                  key={question}
+                >
+                  <Text style={styles.question}>{question}</Text>
+                </Pressable>
               ))}
             </View>
           </>
@@ -58,13 +98,27 @@ const styles = StyleSheet.create({
   description: {
     marginBottom: 20,
     fontSize: 18,
+    lineHeight: 25,
     textAlign: 'left',
     paddingHorizontal: 10,
     paddingVertical: 20,
   },
-  questionContainer: {},
-  questions: {
+  questionContainer: {
+    borderTopWidth: 1,
+    borderTopColor: '#261a1a',
+    marginTop: 5,
+    paddingTop: 10,
+  },
+  questionBtn: {
+    backgroundColor: '#594c4c',
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+    marginBottom: 5,
+    borderRadius: 5,
+  },
+  question: {
     fontSize: 14,
+    color: '#fff',
   },
   cityName: {
     fontFamily: 'Roboto-Bold',
